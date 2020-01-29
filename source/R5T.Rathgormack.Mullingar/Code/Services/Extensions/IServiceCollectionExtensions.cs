67b6f5d60ce450d7f7e2fc287a24d80f1225bd05
MinexAutomation <minex.automation@gmail.com>
2020-01-29 00:22:58 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Rathgormack.Mullingar/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+using R5T.Dacia.Extensions;
+using R5T.Mullingar;
+
+
+namespace R5T.Rathgormack.Mullingar
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddHelloWorldMessenger(this IServiceCollection services,
+            ServiceAction<IHelloWorldMessageProvider> addHelloWorldMessageProvider)
+        {
+            services
+                .AddSingleton<IHelloWorldMessenger, HelloWorldMessenger>()
+                .RunServiceAction(addHelloWorldMessageProvider)
+                ;
+
+            return services;
+        }
+    }
+}
